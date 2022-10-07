840dff42df1ef1ad4f7cc18f9b57940ddff4ab68
Kevin Dew <kevindew@me.com>
2022-10-07 19:11:20 +0100

    Remove routes helpers from GovspeakHelper
    Having these present seems to break routing inside Rails' engines as it
    overrides their Rails routes with the ones from the parent app. We
    experience this using component guide where links seem to end up
    matching an assets route rather than the named route.
    
    For example:
    
    Before
    ```
    (byebug) component_doc_path(component_doc.id)
    "/component-guide/assets/whitehall?action=show&component=layout_for_admin&controller=govuk_publishing_components%2Fcomponent_guide
    ```
    
    After
    ```
    (byebug) component_doc_path(component_doc.id)
    "/component-guide/layout_for_admin
    ```

--- app/helpers/govspeak_helper.rb
+++ app/helpers/govspeak_helper.rb
@@ -2,7 +2,6 @@ require "delegate"
 
 module GovspeakHelper
   include ::Govspeak::ContactsExtractorHelpers
-  include Rails.application.routes.url_helpers
   include LocalisedUrlPathHelper
 
   BARCHART_REGEXP = /{barchart(.*?)}/
