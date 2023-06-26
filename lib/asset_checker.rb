41da087075ec7a83d0f120e760db1653481d72a2
Korny Sietsma <korny.sietsma@digital.cabinet-office.gov.uk>
2023-06-26 11:46:21 +0100

    minor rubocop fixes

--- lib/asset_checker.rb
+++ lib/asset_checker.rb
@@ -1,49 +1,49 @@
 # frozen_string_literal: true
 
-require 'ruby-progressbar'
+require "ruby-progressbar"
 
 class AssetChecker
-  attr_accessor :client
+  attr_accessor :mongodb_client
 
   def initialize
     puts "setting pu client"
-    @client = Mongo::Client.new([ 'mongo-3.6:27017' ], database: 'asset-manager')
-    puts @client
+    @mongodb_client = Mongo::Client.new(["mongo-3.6:27017"], database: "asset-manager")
+    puts @mongodb_client
   end
 
   def close
-    @client.close
+    @mongodb_client.close
   end
 
   def whitehall_asset_state(whitehall_asset)
     attachment = whitehall_asset.significant_attachment(include_deleted_attachables: true)
-    if !attachment
+    unless attachment
       return "missing_attachment"
     end
     if attachment.is_a? Attachment::Null
       return "null_attachment"
     end
+
     edition = Edition.unscoped.find_by_id(attachment.attachable_id)
     edition ? edition.state : "missing_edition"
   end
 
   def add_matching_asset(whitehall_asset, version, am_asset, statistics)
-    key = {version:, whitehall_state: whitehall_asset_state(whitehall_asset), am_state: am_asset["state"], am_draft: am_asset["draft"], whitehall_exists: true, am_exists: true}
+    key = { version:, whitehall_state: whitehall_asset_state(whitehall_asset), am_state: am_asset["state"], am_draft: am_asset["draft"], whitehall_exists: true, am_exists: true }
     statistics[key] += 1
   end
 
   def add_missing_asset(whitehall_asset, version, statistics)
-    key = {version:, whitehall_state:  whitehall_asset_state(whitehall_asset), am_state: nil, am_draft: nil, whitehall_exists: true, am_exists: false}
+    key = { version:, whitehall_state: whitehall_asset_state(whitehall_asset), am_state: nil, am_draft: nil, whitehall_exists: true, am_exists: false }
     statistics[key] += 1
   end
-
 
   def aggregate_statistics(whitehall_asset, statistics)
     versions = whitehall_asset.file.versions.keys
 
     # get base asset
     root_path = whitehall_asset.file.path
-    results = @client[:assets].find({ legacy_url_path: root_path})
+    results = @mongodb_client[:assets].find({ legacy_url_path: root_path })
     if results.count == 1
       add_matching_asset(whitehall_asset, :base, results.first, statistics)
     else
@@ -52,8 +52,9 @@ class AssetChecker
 
     versions.map do |version|
       next unless whitehall_asset.file.versions[version].file
+
       path = whitehall_asset.file.versions[version].file.path
-      results = @client[:assets].find({ legacy_url_path: path})
+      results = @mongodb_client[:assets].find({ legacy_url_path: path })
       if results.count == 1
         add_matching_asset(whitehall_asset, version, results.first, statistics)
       else
@@ -65,22 +66,22 @@ class AssetChecker
   def suppress_sql_logs
     ActiveRecord::Base.logger.level = 1
   end
+
   def restore_sql_logs
     ActiveRecord::Base.logger.level = 0
   end
 
-  def check_all()
+  def check_all
     statistics = Hash.new(0)
 
     total_attachments = AttachmentData.unscoped.count
     max_attachments = 1000 # or total for full run
     puts "Total attachments: #{total_attachments} limiting to #{max_attachments}"
-    progressbar = ProgressBar.create(total: max_attachments, throttle_rate: 0.1, format: " %E	%t: |%B|")
-    AttachmentData.unscoped.take(max_attachments).each_with_index do |attachment, row_index|
+    progressbar = ProgressBar.create!(total: max_attachments, throttle_rate: 0.1, format: " %E	%t: |%B|")
+    AttachmentData.unscoped.take(max_attachments).each_with_index do |attachment, _row_index|
       progressbar.increment
       aggregate_statistics(attachment, statistics)
     end
     pp statistics
   end
-
 end
