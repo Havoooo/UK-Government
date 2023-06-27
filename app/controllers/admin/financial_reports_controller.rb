e5fc7b6a2dbd31035fd0babf13d766016f406ae3
David Trussler <david.trussler@digital.cabinet-office.gov.uk>
2023-06-27 10:53:41 +0100

    Updates controller for design_system

--- app/controllers/admin/financial_reports_controller.rb
+++ app/controllers/admin/financial_reports_controller.rb
@@ -1,11 +1,17 @@
 class Admin::FinancialReportsController < Admin::BaseController
   before_action :load_organisation
   before_action :load_financial_report, only: %i[edit update destroy]
+  layout :get_layout
 
-  def edit; end
+  def index; end
+
+  def edit
+    render_design_system(:edit, :legacy_edit)
+  end
 
   def new
     @financial_report = @organisation.financial_reports.build(year: Time.zone.now.year)
+    render_design_system(:new, :legacy_new)
   end
 
   def create
@@ -31,6 +37,17 @@ class Admin::FinancialReportsController < Admin::BaseController
   end
 
 private
+
+  def get_layout
+    design_system_actions = []
+    design_system_actions += %w[index new create edit update] if preview_design_system?(next_release: false)
+
+    if design_system_actions.include?(action_name)
+      "design_system"
+    else
+      "admin"
+    end
+  end
 
   def load_financial_report
     @financial_report = @organisation.financial_reports.find(params[:id])
